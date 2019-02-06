ef8cd9344c9af76541a73c329bcb53950e709adc
hien hang <hienhang@hotmail.co.uk>
2019-02-06 10:13:48 +0000

    added front page

--- app.py
+++ app.py
@@ -10,33 +10,48 @@ import requests
 import sqlite3
 
 from flask import Flask,render_template,request
-from searchdb import * 
+from searchdb import *
 app = Flask(__name__)
 
 
-
 @app.route("/")
-def home():
-        
-    return render_template("index.html", title = "home" )
+def index():
+    return render_template('index.html', title='home')
 
 
 
-@app.route("/result", methods=["POST"])
-def result():
+@app.route("/business_search.html")
+def business_search():
+    return render_template("business_search.html", title = "search a business" )
+
+
+@app.route("/business_result", methods=["POST"])
+def business_result():
     form_data = request.form
     postcode = form_data["postcode"].upper().replace(' ','')
-    
+
     if postcode:
         geolocation= display50Business(postcode)
-        return render_template("result.html", title = "result", **locals())
+        return render_template("business_result.html", title = "result", **locals())
     else:
         message = "need postcode"
-        return render_template("index.html", title = "result", **locals())
-    
-    
+        return render_template("index.html", title = "business result", **locals())
 
-  
+@app.route("/person_search.html")
+def person_search():
+    return render_template("person_search.html", title="search a person")
+
+@app.route("/person_result", methods=["POST"])
+def person_result():
+    form_data = request.form
+    name = form_data["name"]
+    if name:
+        return render_template("person_result.html", title="person result", **locals())
+    else:
+        message = "No matches found. Sorry!"
+        print (message)
+        return message
+
+
 if __name__ == '__main__':
     app.run(debug =True)
-      
